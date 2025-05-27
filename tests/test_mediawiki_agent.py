1db1660fa2ace0356c07e4904c9fa56ef0660e65
Kyle Marek-Spartz <kyle.marek.spartz@gmail.com>
2025-05-27 17:56:23 -0500

    Add back deleted test

--- tests/test_mediawiki_agent.py
+++ tests/test_mediawiki_agent.py
@@ -2,6 +2,11 @@ import pytest
 
 
 @pytest.mark.skip(reason="Feature not yet implemented")
+def test_category_operations_placeholder():
+    pass
+
+
+@pytest.mark.skip(reason="Feature not yet implemented")
 def test_access_page_history_placeholder():
     pass
 
